769825bf043b232035ff55f2ef1242e0a78d23d1
LeBrontoJames <keenanabigailidn@gmail.com>
2025-02-24 10:34:37 +0700

    Gemini Service API

---
+++ lib/service/gemini_service.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_schedule_generator/models/task.dart';
+import 'package:http/http.dart' as http;
+
+class GeminiService {
+  static const String _baseUrl =
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
+  final String apiKey;
+  GeminiService() : apiKey = dotenv.env["GEMINI_API_KEY"] ?? "" {
+    if (apiKey.isEmpty) {
+      throw ArgumentError('API key cannot is missing');
+    }
+  }
+  Future<String> generateSchedule(List<Task> tasks) async {
+    _validateTasks(tasks);
+    final prompt = _buildPrompt(tasks);
+    try {
+      print("Prompt: \n$prompt");
+      final response = await http.post(Uri.parse('$_baseUrl?key=$apiKey'),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode({
+            {
+              "contents": [
+                {
+                  "parts": [
+                    {"text": prompt}
+                  ]
+                }
+              ]
+            }
+          }));
+      return _handleResponse(response);
+    } catch (e) {
+      throw ArgumentError("Failed to generate schedule: $e");
+    }
+  }
+
+  String _handleResponse(http.Response response) {
+    final data = jsonDecode(response.body);
+    if (response.statusCode == 401) {
+      throw ArgumentError("Invalid API Key or Unauthorized Access");
+    } else if (response.statusCode == 429) {
+      throw ArgumentError("Rate Limit Exceeded");
+    } else if (response.statusCode == 500) {
+      throw ArgumentError("Internal Server Error");
+    } else if (response.statusCode == 503) {
+      throw ArgumentError("Service Unavailable");
+    } else if (response.statusCode == 200) {
+      return data["contents"][0]["parts"][0]["text"];
+    } else {
+      throw ArgumentError("Unknown Error");
+    }
+  }
+
+  String _buildPrompt(List<Task> tasks) {
+    final tasksList = tasks
+        .map((task) =>
+            "${task.name} (Priority: ${task.priority}, Duration: ${task.duration}, Deadline: ${task.deadline})")
+        .join("\n");
+    return "Buatkan Jadwal Harian yang optimal berdasarkan task berikut:\n$tasksList";
+  }
+
+  void _validateTasks(List<Task> tasks) {
+    if (tasks.isEmpty) {
+      throw ArgumentError('Tasks cannot be empty');
+    }
+  }
+}
